768e1d8e2cc896c355b7d4a188a1787aa7644555
electron378 <electron378@gmail.com>
2014-07-27 13:11:06 +0200

    Update parseCsvFolder.py
    empty CSV folder parser sandbox

--- parseCsvFolder.py
+++ parseCsvFolder.py
@@ -5,46 +5,20 @@ import sys
 import csv
 import os
 
-class CLIApp(object):
-
-    def showUsage(self):
-        print "How to use this script: ./cli_app.py put_k_papke"
-
-    def parsniPapku(self, put_k_papke):
-        for fname in os.listdir(put_k_papke):
-            if not fname.endswith('.csv'): continue
-            fullname = os.path.join(put_k_papke, fname)
-            print "{0}; {1}".format(fname, self.parseMyFile(fullname))
-
-    def parseMyFile(self, fname):
-     col_reactionTime = 126
-     col_trialName = 98
-     col_isCorrect = 110 # fix tis one with correct column coord
+def theApp():
+    # define constants here
+    path_to_folder = sys.argv[1] # assuming th user is smart enough to pass the argument
     
-        output = []
-        fh = open(fname, 'rb')
-csvshechka = csv.reader(fh)
-        for row in csvshechka:
-            chislo = 0
-            try:
-                chislo = int(row[col_reactionTime])
-            except:
-                pass
-            if chislo > 0:
-                if 'S' in row[col_trialName]:
-                    chislo += 300
-                cellText = str(chislo)
-                if '0' in row[col_isCorrect]:
-                    cellText = ' '
-                output.append(cellText)
-        fh.close()
-        return "; ".join(output)
-
-    def __init__(self):
-        if len(sys.argv) < 2:
-self.showUsage();
-        else:
-            print self.parsniPapku(sys.argv[1])
+    # here comes the code
+    for fname in os.listdir(path_to_folder):
+        if not fname.endswith('.csv'): continue
+        fullname = os.path.join(put_k_papke, fname)
+        
+        # now we handle the file (we know it as fname or fullname)
+        output = "results go here!"
+        
+        # time to output!
+        print "{0}; {1}".format(fname, output)
 
 if __name__ == "__main__":
-    CLIApp()
+    theApp()
