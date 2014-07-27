fae0f8fbb1852421b3b891ba6502ecb9570d5159
electron378 <electron378@users.noreply.github.com>
2014-07-27 13:36:16 +0200

    Update parseCsvFolder.py

--- parseCsvFolder.py
+++ parseCsvFolder.py
@@ -8,6 +8,8 @@ import os
 def theApp():
     # define constants here
     path_to_folder = sys.argv[1] # assuming th user is smart enough to pass the argument
+    column_reaction_time = 126
+    column_correct_response = 42
     
     # here comes the code
     for fname in os.listdir(path_to_folder):
@@ -19,9 +21,17 @@ def theApp():
         fh = open(fullname, 'rb') # opening .csv file
         csv_reader = csv.reader(fh) # reading csv
         for row in csv_reader:
+            
             # here we handle the row
-            # TODO
-            pass
+            # WARNING: if your .csv contains text entries (like headers) handle them here
+            reaction_time = int(row[column_reaction_time]) # here we get the raw RT as a number
+            # altering of RT should hppen here
+            cell_text = str(chislo) # saving the RT as text for output
+            if '0' in row[column_correct_response]: # describe what happens if response is wrong below
+                cellText = ' ' # replace reaction time value with nothing (write empty cell)
+                
+            output_cells.append(cell_text) # saving the final value to output stream
+            
         fh.close() # closing .csv file
         
         # time to output!
